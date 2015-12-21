da394cd903eb45445a403773b4e2be7643af4698
Julien Kervizic <julienk@julienk-mbp.local>
2015-12-21 14:00:43 +0000

    fixing some moree... :(

--- CrawlerDownloader.py
+++ CrawlerDownloader.py
@@ -1,18 +1,23 @@
-from Tkinter import * 
-from ttk import Frame, Button, Label, Style
+import os, re, requests, json, threading
+from ttk import Frame, Button, Label, Style, Combobox
 from tkFileDialog import askdirectory
-import os, re, requests, json
+from Tkinter import * 
 
 def crawl(url, fileformat):
-    x = requests.get(url)
-    z = [y for y in x]
-    z2 = ''.join(z)
-    format_url = '(http[^\"|^\']+.' + fileformat + '([^\"|^\'|^\b]+)?)'
-    output = re.findall(format_url , z2)
-    return [x[0] for x in output]
+	x = requests.get(url)
+	z = [y for y in x]
+	z2 = ''.join(z)
+	format_url = '(http[^\"|^\']+.' + fileformat + '([^\"|^\'|^\b]+)?)'
+	output = re.findall(format_url , z2)
+	return [x[0] for x in output]
+
+def crawler(url, fileformat):
+    x = threading.Thread(name='crawl', target=crawl, args=(url,fileformat))
+    x.start()
+    return x
 
 def url_cleanup(url):
-    return json.loads('"'+ url + '"')
+	return json.loads('"'+ url + '"')
 
 def download_file(url):
     local_filename = url.split('/')[-1]
@@ -25,8 +30,8 @@ def download_file(url):
     return local_filename
 
 def fast_dl(inputurl):
-    url = url_cleanup(inputurl)
-    return download_file(url)
+	url = url_cleanup(inputurl)
+	return download_file(url)
 
 def thread_dl(inputurl):
     dlThread = threading.Thread(
@@ -35,6 +40,13 @@ def thread_dl(inputurl):
         args=(''.join(inputurl), )
         )
     dlThread.start()
+
+class configuration():
+    def __init__(self):
+        self._tf_height = 2
+        self._tf_width = 50
+
+config = configuration()
 
 class EntryBoxFrame(Frame):
     def __init__(self, parent):
@@ -82,7 +94,7 @@ class ListBoxFrame(Frame):
         x = self.area.curselection()
         return self.area.data[x[0]]
 
-class DropdownFrame(Frame):
+class Dropdownframe(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)   
         self.parent = parent  
@@ -110,8 +122,8 @@ class Mainframe(Frame):
 
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
+        #self.rowconfigure(3, weight=1)
+        #self.rowconfigure(6, pad=7)
         
         lbl = Label(self, text="Placeholder")
         lbl.grid(sticky=W, pady=4, padx=5)
@@ -119,8 +131,8 @@ class Mainframe(Frame):
         abtn = Button(self, text="Fetch", command=self.update_url_list)
         abtn.grid(row=2, column=3)
         cbtn = Button(self, text="Download", command=self.download_url)
-        cbtn.grid(row=3, column=3, pady=4)
-        self.dd = DropdownFrame(self)
+        cbtn.grid(row=3, column=3, pady=0)
+        self.dd = Dropdownframe(self)
         self.EB = EntryBoxFrame(self)
         self.LB = ListBoxFrame(self)
     def update_url_list(self):
